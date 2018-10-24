c36047b27475c3155136053cdaf2d70a715a9227
Alexander Dahl <aleda145@student.liu.se>
2018-10-24 22:17:45 +0200

    Add new file

---
+++ lab1/lab1.sql
@@ -0,0 +1,16 @@
+/*
+Lab 1 report <Student_names and liu_id>
+*/
+
+/*
+Drop all user created tables that have been created when solving the lab
+*/
+
+DROP custom_table;
+
+
+/*
+Question 0: Print a message that says "hello world"
+*/
+
+SELECT 'hello world!' AS 'message'
